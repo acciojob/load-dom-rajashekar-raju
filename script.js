849da3263f6cf020c3958b499cf32675562cd1b5
rajashekar-raju <96567852+rajashekar-raju@users.noreply.github.com>
2024-01-24 12:04:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-let p = document.createElemnt("p");
+let p = document.createElement("p");
 document.body.addEventListner("load",()=>{
 	p.innerText="DOM load success"
 })
